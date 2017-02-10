4785e7fad6bbb1120844c3742d30fb2f1f9f1822
Donnie Ellstrom <dsvene@gmail.com>
2017-02-09 18:10:10 -0800

    Hookup API

--- static/app/containers/home/home.js
+++ static/app/containers/home/home.js
@@ -5,18 +5,39 @@ angular.module('App')
   controllerAs: 'homeComp'
 });
 
-function HomeCompCtrl(WordSearch) {
+function HomeCompCtrl($http) {
   var homeComp = this;
+
+  // homeComp.searchWord = function(word) {
+  //   console.log("search term: ", homeComp.wordSearchTerm)
+  //   if (homeComp.wordSearchTerm !== undefined && homeComp.wordSearchTerm !== "") {
+  //     var serviceWord = homeComp.wordSearchTerm;
+  //     WordSearch().then(function(response) {
+  //       console.log(response)
+  //     })
+  //   }
+  // }
 
   homeComp.searchWord = function(word) {
     console.log("search term: ", homeComp.wordSearchTerm)
     if (homeComp.wordSearchTerm !== undefined && homeComp.wordSearchTerm !== "") {
       var serviceWord = homeComp.wordSearchTerm;
-      WordSearch.search(serviceWord).then(function(words) {
-        console.log(words)
-      })
+      // $http.defaults.headers.common['X-Mashape-Key'] = '2Io5M1agBJmshkIdUw0kotDZYTWQp1aL0GajsnzOU6V7tiejJ1';
+      var req = {
+        method: "GET",
+        url: 'https://mashape-community-urban-dictionary.p.mashape.com/define?term=wat',
+        headers: {
+        'X-Mashape-Key': '2Io5M1agBJmshkIdUw0kotDZYTWQp1aL0GajsnzOU6V7tiejJ1',
+        'Accept': "application/json"
+        }
+      };
+        $http(req)
+        .then(function(response) {
+            console.log(response);
+            return response;
+        });
     }
   }
 
 }
-HomeCompCtrl.$inject = ['WordSearch'];
+HomeCompCtrl.$inject = ['$http'];
--- static/app/services/services.js
+++ static/app/services/services.js
@@ -1,12 +1,23 @@
 angular.module('App')
 
-  .factory('WordSearch', ['$http', function($http) {
-    return {
-      search: function(serviceWord) {
-        var URL = '/api/searchword/' + serviceWord;
-        var req = {
+  // .factory('WordSearch', ['$http', function($http) {
+  //   return {
+  //     search: function(serviceWord) {
+  //       var URL = "https://mashape-community-urban-dictionary.p.mashape.com/define?term=wat"
+  //       var req = {
+  //         url: URL,
+  //         method: "GET"
+  //       };
+  //       console.log("In factory: ", res)
+  //       return $http(res)
+  //     }
+  //   };
+  // }])
 
-        }
-      }
-    }
-  }])
+  // .factory('WordSearch', ['$http', function($http) {
+  //       $http.get("https://mashape-community-urban-dictionary.p.mashape.com/define?term=wat")
+  //       .then(function(response) {
+  //         console.log(response)
+  //         return response;
+  //       })
+  // }]);
